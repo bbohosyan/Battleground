2a44abb1f62317a4714e8ba33448a6c1c5321f71
siqqQ <b.bohosyan@gmail.com>
2018-08-07 17:27:50 +0300

    Initial commit

---
+++ app/src/main/java/com/battleground/battleground/fragments/LoginFragment.java
@@ -0,0 +1,34 @@
+package com.battleground.battleground.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.battleground.battleground.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LoginFragment extends Fragment {
+
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    public static LoginFragment instance(){
+        LoginFragment loginFragment = new LoginFragment();
+        return loginFragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+}
