9f004e25d7b117802c4b61cfbd9c5247b4404cb7
siqqQ <b.bohosyan@gmail.com>
2018-08-20 11:46:54 +0300

    Refactor ShopActivity

--- app/src/main/java/com/battleground/battleground/activities/ShopActivity.java
+++ app/src/main/java/com/battleground/battleground/activities/ShopActivity.java
@@ -2,17 +2,12 @@ package com.battleground.battleground.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.battleground.battleground.R;
 import com.battleground.battleground.fragments.ShopFragment;
@@ -22,78 +17,14 @@ import com.google.firebase.auth.FirebaseAuth;
 public class ShopActivity extends AppCompatActivity implements Navigator {
 
     private DrawerLayout mDrawerLayout;
-    private Bundle mBundle;
     private FirebaseAuth mAuth;
-    private Navigator navigator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
-        mDrawerLayout = findViewById(R.id.activity_shop_drawer);
-        mDrawerLayout.addDrawerListener(
-                new DrawerLayout.DrawerListener() {
-                    @Override
-                    public void onDrawerSlide(View drawerView, float slideOffset) {
-                        // Respond when the drawer's position changes
-                    }
-
-                    @Override
-                    public void onDrawerOpened(View drawerView) {
-                        // Respond when the drawer is opened
-                    }
-
-                    @Override
-                    public void onDrawerClosed(View drawerView) {
-                        // Respond when the drawer is closed
-                    }
-
-                    @Override
-                    public void onDrawerStateChanged(int newState) {
-                        // Respond when the drawer motion state changes
-                    }
-                }
-        );
-
-        NavigationView navigationView = findViewById(R.id.activity_overview_nav_view);
-        navigationView.setCheckedItem(R.id.nav_shop);
-        navigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(MenuItem menuItem) {
-                        // set item as selected to persist highlight
-                        switch (menuItem.getItemId()){
-                            case R.id.nav_profile:
-                                navigateToOverviewActivity();
-                                break;
-                            case R.id.nav_shop:
-                                navigateToShopActivity();
-                                break;
-                            case R.id.nav_battle:
-                                navigateToBattleActivity();
-                                break;
-                        }
-                        // close drawer when item is tapped
-                        mDrawerLayout.closeDrawers();
-                        //navigator.navigateToShopActivity();
-                        // Add code here to update the UI based on the item selected
-                        // For example, swap UI fragments here
-
-                        return true;
-                    }
-                });
-
-
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-
-        View headerView = navigationView.getHeaderView(0);
-        TextView navUsername = (TextView) headerView.findViewById(R.id.nav_head_text);
-
-        mAuth = FirebaseAuth.getInstance();
-        navUsername.setText(mAuth.getCurrentUser().getEmail().toString());
-
-        mDrawerLayout.bringToFront();
+        
+        setDrawer();
 
         ShopFragment registerFragment = ShopFragment.instance();
         registerFragment.setNavigator(this);
@@ -128,7 +59,6 @@ public class ShopActivity extends AppCompatActivity implements Navigator {
     @Override
     public void navigateToLoginActivity() {
         Intent intent = new Intent(this, LoginActivity.class);
-        //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
 
@@ -166,5 +96,37 @@ public class ShopActivity extends AppCompatActivity implements Navigator {
     public void navigateToBattleActivity() {
         Intent intent = new Intent(this, BattleActivity.class);
         startActivity(intent);
+    }
+
+    private void setDrawer() {
+        mDrawerLayout = findViewById(R.id.activity_shop_drawer);
+
+        NavigationView navigationView = findViewById(R.id.activity_overview_nav_view);
+        navigationView.setCheckedItem(R.id.nav_shop);
+        navigationView.setNavigationItemSelectedListener(
+                menuItem -> {
+                    switch (menuItem.getItemId()){
+                        case R.id.nav_profile:
+                            navigateToOverviewActivity();
+                            break;
+                        case R.id.nav_shop:
+                            navigateToShopActivity();
+                            break;
+                        case R.id.nav_battle:
+                            navigateToBattleActivity();
+                            break;
+                    }
+                    mDrawerLayout.closeDrawers();
+                    return true;
+                });
+
+        View headerView = navigationView.getHeaderView(0);
+        TextView navUsername = headerView.findViewById(R.id.nav_head_text);
+
+        mAuth = FirebaseAuth.getInstance();
+        navUsername.setText(mAuth.getCurrentUser().getEmail().toString());
+
+        mDrawerLayout.bringToFront();
+
     }
 }
