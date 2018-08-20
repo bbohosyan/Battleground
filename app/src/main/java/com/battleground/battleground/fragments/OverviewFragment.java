0df26a671b8eb8a26d71857f2fe4298b9b7737de
siqqQ <b.bohosyan@gmail.com>
2018-08-20 12:16:39 +0300

    Refactor OverviewFragment

--- app/src/main/java/com/battleground/battleground/fragments/OverviewFragment.java
+++ app/src/main/java/com/battleground/battleground/fragments/OverviewFragment.java
@@ -5,19 +5,12 @@ import android.os.Bundle;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -32,12 +25,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import org.w3c.dom.Text;
-
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.temporal.ChronoUnit;
-import java.util.Date;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -51,8 +41,6 @@ public class OverviewFragment extends Fragment {
     private DatabaseReference myRef;
     private String userID;
     private static Bundle mExtras;
-    private TextView mTextView;
-    private TextView mNavHeaderTitle;
     private DrawerLayout mDrawerLayout;
     private User mUser;
     private TextView mEmailTextView;
@@ -82,7 +70,6 @@ public class OverviewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_overview, container, false);
 
         mAuth = FirebaseAuth.getInstance();
@@ -91,15 +78,12 @@ public class OverviewFragment extends Fragment {
         FirebaseUser user = mAuth.getCurrentUser();
         userID = user.getUid();
 
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    Toast.makeText(getContext(), "SUCCESS", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(getContext(), "ERROR", Toast.LENGTH_SHORT).show();
-                }
+        mAuthListener = firebaseAuth -> {
+            FirebaseUser user1 = firebaseAuth.getCurrentUser();
+            if (user1 != null) {
+                Toast.makeText(getContext(), "SUCCESS", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getContext(), "ERROR", Toast.LENGTH_SHORT).show();
             }
         };
 
@@ -115,18 +99,10 @@ public class OverviewFragment extends Fragment {
             }
         });
 
-//        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
-//        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-                mDrawerLayout = getActivity().findViewById(R.id.drawer_layout);
-                mDrawerLayout.openDrawer(GravityCompat.START);
-            }
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+        fab.setOnClickListener(view1 -> {
+            mDrawerLayout = getActivity().findViewById(R.id.drawer_layout);
+            mDrawerLayout.openDrawer(GravityCompat.START);
         });
 
         mEmailTextView = view.findViewById(R.id.fragment_overview_email);
@@ -149,7 +125,6 @@ public class OverviewFragment extends Fragment {
         for (DataSnapshot ds : dataSnapshot.getChildren()) {
             mUser = ds.child(userID).getValue(User.class);
             LocalDate birthday = LocalDate.of(1996, 2, 29);
-            int age = calculateAge(birthday, LocalDate.now());
             mAgeTextView.setText("Age: " + String.valueOf(mUser.getYearsUser()));
             String[] dateUserRegistered = mUser.getDateUserRegistered().split("-");
             int yearUserRegistered = Integer.parseInt(dateUserRegistered[0]);
@@ -176,14 +151,6 @@ public class OverviewFragment extends Fragment {
             mGoldWon.setText("All time gold won: " + mUser.getStatistics().getGoldWon());
 
             mDamageDealt.setText("All time damage dealt: " + mUser.getStatistics().getDamageDealt());
-        }
-    }
-
-    public static int calculateAge(LocalDate birthDate, LocalDate currentDate) {
-        if ((birthDate != null) && (currentDate != null)) {
-            return Period.between(birthDate, currentDate).getYears();
-        } else {
-            return 0;
         }
     }
 
